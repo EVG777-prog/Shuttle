ace0cfcd117f8fc223f9daed0b6e08602e31d970
Evgeniy <85104881+EVG777-prog@users.noreply.github.com>
2024-01-26 00:11:44 +0200

    add signUp.js to all pages and class apply-button

--- SignUp.js
+++ SignUp.js
@@ -1,23 +1,25 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+// Получаем модальное окно
+const modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("apply-button");
+// Получаем все кнопки, которые открывают модальное окно
+const btns = document.querySelectorAll(".apply-button");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close-button")[0];
+// Получаем элемент <span>, который закрывает модальное окно
+const span = document.getElementsByClassName("close-button")[0];
 
-// When the user clicks the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
-};
+// Добавляем обработчик событий для каждой кнопки
+btns.forEach(function (btn) {
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+});
 
-// When the user clicks on <span> (x), close the modal
+// Когда пользователь кликает на <span> (x), закрыть модальное окно
 span.onclick = function () {
   modal.style.display = "none";
 };
 
-// When the user clicks anywhere outside of the modal, close it
+// Когда пользователь кликает вне модального окна, закрыть его
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
